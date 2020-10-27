1edf1f937b77812a28bf986f49731b4736493adb
wyao <289672494@qq.com>
2020-10-27 21:01:44 +0800

    [wang yao] feature: 获取后台传来的学员列表

--- src/App/components/PersonList/index.js
+++ src/App/components/PersonList/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class PersonList extends Component {
   constructor(props) {
@@ -9,13 +10,10 @@ class PersonList extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      persons: [
-        {
-          id: 1,
-          name: 'test',
-        },
-      ],
+    axios.get('http://127.0.0.1:8080/students').then((response) => {
+      this.setState({
+        persons: response.data,
+      });
     });
   }
 
